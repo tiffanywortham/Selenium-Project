7355cf8bfdf26385b49c86233a27f67600c5e5d1
Taylor Cook <tmc97@live.missouristate.edu>
2018-11-04 16:44:09 -0600

    change js location of no button

--- assets/cheesefunction.js
+++ assets/cheesefunction.js
@@ -10,7 +10,7 @@ let loadFunction = function() {
 
     let changeNoButton = document.querySelector('.no-button');
     let changeNo = function() {
-        window.location.replace("http://google.com");
+        window.location.replace("https://www.google.com/search?q=grapes&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi629Ly57veAhWI6oMKHdWRDqUQ_AUIEygB&biw=1678&bih=867&dpr=2");
     };
     changeNoButton.addEventListener("click", changeNo);
 
