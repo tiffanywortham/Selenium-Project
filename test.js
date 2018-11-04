faa8568909cff9db12525083d187feee2da60531
tiffanywortham <tiffanywortham33@gmail.com>
2018-11-04 15:03:33 -0600

    Restructured test cases into a series of try/catch statements

--- test.js
+++ test.js
@@ -1,23 +1,56 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  try {
-    await driver.get('localhost:3000');
-    await driver.wait(until.titleIs('Top 10 crackers yo'), 1000);
-    console.log("success at the desk");
-
-    await driver.wait(until.elementLocated(By.id('inputCracker')), 20000);  
-    await driver.findElement(By.id('inputCracker')).sendKeys('haha yeet', Key.RETURN);
+    let driver = await new Builder().forBrowser('chrome').build();
     
-    //await driver.wait(until.elementLocated(By.id('submitButton')), 20000);  
-    await driver.findElement(By.id("submitButton"));
-    await driver.executeScript("arguments[0].click();", driver.findElement(By.id("submitButton")));
-      
-    await driver.findElement(By.name("cheeseLink")).click();
-    await driver.wait(until.titleIs("Cheese: The bee's knees"), 1000);
+    try {
+        await driver.get('localhost:3000');  
+        await driver.wait(until.titleIs('Top 10 crackers yo'), 1000);
+        console.log("Test case 1 passed");
+    } catch(err){
+        console.log("Test case 1 failed with error: ", err);
+    }
     
-  } finally {
+    try{
+        await driver.wait(until.elementLocated(By.id('inputCracker')), 20000);  
+        await driver.findElement(By.id('inputCracker')).sendKeys('haha yeet', Key.RETURN);
+        console.log("Test case 2 passed");
+    } catch(err){
+        console.log("Test case 2 failed with error: ", err);
+    }
+    
+    try{
+        await driver.findElement(By.id("submitButton"));
+        await driver.executeScript("arguments[0].click();", driver.findElement(By.id("submitButton")));
+        console.log("Test case 3 passed");
+    } catch(err){
+        console.log("Test case 3 failed with error: ", err);
+    }
+    
+    try{
+        await driver.findElement(By.name("cheeseLink")).click();
+        await driver.wait(until.titleIs("Ceese: The bee's knees"), 1000);
+        console.log("Test case 4 passed");
+    } catch(err){
+        console.log("Test case 4 failed with error: ", err);
+    }
+          
+    try{
+        await driver.findElement(By.name("yesButton")).click();
+        console.log("Test case 5 passed");
+    } catch(err){
+        console.log("Test case 5 failed with error: ", err);
+    }
+    
+    try{
+        await driver.findElement(By.name("homeLink")).click();
+        await driver.wait(until.titleIs('Top 10 crackers yo'), 1000);
+        console.log("Test case 6 passed");
+    } catch(err){
+        console.log("Test case 6 failed with error: ", err);
+    }
+    
+    finally {
     await driver.quit();
   }
 })();
